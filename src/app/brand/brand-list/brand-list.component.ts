eb3a62812b1a51a9e30d8b5331ff5bd760194c7c
Balu s <balu.s@transneuron.com>
2018-12-30 22:51:17 +0530

    list page created till store

--- src/app/brand/brand-list/brand-list.component.ts
+++ src/app/brand/brand-list/brand-list.component.ts
@@ -6,10 +6,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brand-list.component.css']
 })
 export class BrandListComponent implements OnInit {
-
+  length = 50;
+  vendorList =  [
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  assignPageNo(pageNo) {
+    console.log(pageNo);
+  }
+
+  assignRecordsPerPage(recordsPerPage) {
+    console.log(recordsPerPage);
+  }
 }
--- src/app/store/store-list/store-list.component.ts
+++ src/app/store/store-list/store-list.component.ts
@@ -6,10 +6,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store-list.component.css']
 })
 export class StoreListComponent implements OnInit {
-
+  length = 50;
+  vendorList =  [
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
+  assignPageNo(pageNo) {
+    console.log(pageNo);
+  }
 
+  assignRecordsPerPage(recordsPerPage) {
+    console.log(recordsPerPage);
+  }
 }
