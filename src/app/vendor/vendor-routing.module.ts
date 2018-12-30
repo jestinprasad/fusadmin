bb3e46babf29d9d6ca82d56c553ddcb4a9a29aa0
Balu s <balu.s@transneuron.com>
2018-12-30 21:42:04 +0530

    changes updated

--- src/app/vendor/vendor-routing.module.ts
+++ src/app/vendor/vendor-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VendorListComponent } from './vendor-list/vendor-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateVendorComponent } from './create-vendor/create-vendor.component';
 
 
 const routes: Routes = [{
-  path: '', component: VendorListComponent}];
+  path: '', component: VendorListComponent
+}, {
+  path: 'create', component: CreateVendorComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
