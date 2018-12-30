f9c7d5a281d872dfec1941fe71e0cf82cd5080df
prasad.v@transneuron.com <prasad.v@transneuron.com>
2018-12-31 00:44:46 +0530

    listing page upto offers completed

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -7,9 +7,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  length = 50;
+  vendorList =  [
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    },
+    {
+      name: 'balu' ,
+      email: 'balu@gmail.com',
+      phoneNo: 8300187422,
+      vendor_id : 61
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  assignPageNo(pageNo) {
+    console.log(pageNo);
+  }
+
+  assignRecordsPerPage(recordsPerPage) {
+    console.log(recordsPerPage);
   }
 
 }
