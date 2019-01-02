170faea122ba8e8a2b32a6ffddd11ebbe5ac4abf
Balu s <balu.s@transneuron.com>
2019-01-02 10:27:08 +0530

    store create page updated

--- src/app/store/store-routing.module.ts
+++ src/app/store/store-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { StoreListComponent } from './store-list/store-list.component';
+import { CreateStoreComponent } from './create-store/create-store.component';
 
 
 
 const routes: Routes = [{
-  path: '', component: StoreListComponent}];
+  path: '', component: StoreListComponent},
+  {
+    path: 'create', component: CreateStoreComponent
+  }];
 
 
 @NgModule({
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { StoreListComponent } from './store-list/store-list.component';
 import { StoreRoutingModule } from './store-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CreateStoreComponent } from './create-store/create-store.component';
 
 @NgModule({
-  declarations: [StoreListComponent],
+  declarations: [StoreListComponent, CreateStoreComponent],
   imports: [
     CommonModule,
     StoreRoutingModule,
